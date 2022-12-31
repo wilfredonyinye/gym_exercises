423c60f2c1c08b02c53c77b131945f3160102f19
Onyinye <wilfredonyinye74@gmail.com>
2022-12-31 14:45:59 +0100

    changes

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,8 +1,8 @@
 export const exerciseOptions = {
     method: 'GET',
     headers: {
-      // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Key': '7baab676fbmshd011b73dc9109d8p1bc689jsnb7da8aebf142',
+      'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      // 'X-RapidAPI-Key': '7baab676fbmshd011b73dc9109d8p1bc689jsnb7da8aebf142',
       'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
     }
 };
@@ -10,8 +10,8 @@ export const exerciseOptions = {
 export const youtubeOptions = {
   method: 'GET',
   headers: {
-    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-    'X-RapidAPI-Key': '7baab676fbmshd011b73dc9109d8p1bc689jsnb7da8aebf142',
+    'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    // 'X-RapidAPI-Key': '7baab676fbmshd011b73dc9109d8p1bc689jsnb7da8aebf142',
     'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
   }
 };
