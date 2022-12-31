4d27ef1694c0eb0492f5f17ff06d85ae3618fa94
Adedolapo <adedolapo@glade.ng>
2022-12-31 11:30:28 +0100

    Gym exercise App

---
+++ src/utils/fetchData.js
@@ -0,0 +1,24 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+      'X-RapidAPI-Key': '7baab676fbmshd011b73dc9109d8p1bc689jsnb7da8aebf142',
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com'
+    }
+};
+  
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    // 'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
+    'X-RapidAPI-Key': '7baab676fbmshd011b73dc9109d8p1bc689jsnb7da8aebf142',
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com'
+  }
+};
+
+export const fetchData = async (url, options) => {
+  const response = await fetch(url, options);
+  const data = await response.json();
+
+  return data;
+}
